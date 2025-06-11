86b6edcc51257a777415d7331086dcdabdc8bee4
Aadish1801 <154327863+Aadish1801@users.noreply.github.com>
2025-06-11 22:49:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.py
@@ -0,0 +1,12 @@
+class Solution:
+  def clearDigits(self, s: str) -> str:
+    ans = []
+
+    for c in s:
+      if c.isdigit():
+.
+        ans.pop()
+      else:
+        ans.append(c)
+
+    return ''.join(ans)
